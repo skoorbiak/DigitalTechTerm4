468ad4d3cb78bc98b3fb29cc1b7d07aed00f3767
skoorbiak <kaibrooks2@outlook.com>
2021-10-27 12:30:00 +1000

    version 1.3

--- app/src/main/java/com/example/digitaltechnologyassessmentterm4/settings_screen.java
+++ app/src/main/java/com/example/digitaltechnologyassessmentterm4/settings_screen.java
@@ -2,30 +2,60 @@ package com.example.digitaltechnologyassessmentterm4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class settings_screen extends AppCompatActivity {
     int minteger = 70;
+    MediaPlayer mp;
+    MediaPlayer mp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_screen);
+        mp = MediaPlayer.create(settings_screen.this, R.raw.alertnotification);
+        mp2 = MediaPlayer.create(settings_screen.this, R.raw.windthroughtreessfx);
+        mp2.start();
     }
 
-    public void increaseVol(View view) {
+    public void increaseVolSFX(View view) {
         minteger = minteger + 5;
+        mp.start();
         display(minteger);
     }
 
-    public void decreaseVol(View view) {
+    public void decreaseVolSFX(View view) {
         minteger = minteger - 5;
+        mp.start();
         display(minteger);
     }
-    private void display(int number) {
+    public void display(int number) {
         TextView displayInteger = (TextView) findViewById(
                 R.id.sfxvolume);
         displayInteger.setText("" + number);
+    }
+    public void display2(int number2) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.volume_text_view);
+        displayInteger.setText("" + number2);
+    }
+    public void increaseVol(View view) {
+        minteger = minteger + 5;
+        mp.start();
+        display2(minteger);
+    }
+    public void decreaseVol(View view){
+        minteger = minteger - 5;
+        mp.start();
+        display2(minteger);
+    }
+
+    public void back2mainscreen_btn(View view) {
+        Intent intent = new Intent(this,main_menu_screen.class);
+        mp.start();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/digitaltechnologyassessmentterm4/shop_screen.java
+++ app/src/main/java/com/example/digitaltechnologyassessmentterm4/shop_screen.java
@@ -2,13 +2,37 @@ package com.example.digitaltechnologyassessmentterm4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class shop_screen extends AppCompatActivity {
-
+    MediaPlayer mp;
+    MediaPlayer mp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop_screen);
+        mp = MediaPlayer.create(shop_screen.this, R.raw.alertnotification);
+        mp2 = MediaPlayer.create(shop_screen.this, R.raw.windthroughtreessfx);
+        mp2.start();
+    }
+
+    public void playStore1(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=yourpackegName")); /// here "yourpackegName" from your app packeg Name
+        startActivity(intent);
+    }
+
+    public void playStore2(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("market://details?id=yourpackegName")); /// here "yourpackegName" from your app packeg Name
+        startActivity(intent);
+    }
+    public void back2main(View view){
+        Intent intent = new Intent(this,shop_screen.class);
+        mp.start();
+        startActivity(intent);
     }
 }
