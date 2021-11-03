a0200b3b5a9177d77fb827769ed396ec7d95b403
skoorbiak <kaibrooks2@outlook.com>
2021-11-03 12:29:05 +1000

    version 1.4

--- app/src/main/java/com/example/digitaltechnologyassessmentterm4/play_screen.java
+++ app/src/main/java/com/example/digitaltechnologyassessmentterm4/play_screen.java
@@ -2,13 +2,34 @@ package com.example.digitaltechnologyassessmentterm4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class play_screen extends AppCompatActivity {
-
+    MediaPlayer mp;
+    MediaPlayer mp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_screen);
+        mp = MediaPlayer.create(play_screen.this, R.raw.alertnotification);
+        mp2 = MediaPlayer.create(play_screen.this, R.raw.windthroughtreessfx);
+        mp2.start();
+    }
+
+    public void go2songone(View view) {
+        Intent intent = new Intent(this,song_one.class);
+        mp.start();
+        mp2.stop();
+        startActivity(intent);
+
+    }
+
+    public void backtomain(View view) {
+        Intent intent = new Intent (this, main_menu_screen.class);
+        mp.start();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/digitaltechnologyassessmentterm4/song_one.java
+++ app/src/main/java/com/example/digitaltechnologyassessmentterm4/song_one.java
@@ -2,13 +2,32 @@ package com.example.digitaltechnologyassessmentterm4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class song_one extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class song_one extends AppCompatActivity {
+    Timer timer;
+    MediaPlayer mp;
+    MediaPlayer mp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_one);
+        mp = MediaPlayer.create(song_one.this, R.raw.alertnotification);
+        mp2 = MediaPlayer.create(song_one.this, R.raw.sparkoflightsong);
+        mp2.start();
+        timer = new Timer();
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+            Intent intent = new Intent (song_one.this, win_screen.class);
+            startActivity(intent);
+            mp.start();
+            }
+        }, 165000);
     }
 }
